9460d501670be306ed6d7e9c77d2c6d520897308
Jack Andrews <japotts16@gmail.com>
2021-10-11 11:58:34 +0100

    add assets

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "antd/dist/antd.css";
 import Script from "next/script";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
-import  Layout from "../components/Layout";
+import Layout from "../components/Layout";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,31 @@ function MyApp({ Component, pageProps }) {
       <QueryClientProvider client={queryClient}>
         <Head>
           <title>opensend.me</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
 
         <Layout>
